4001ce0f97c4748077ec10c5fb1be650517b69d1
Juan Pinto <jpinto@grupov.com.ve>
2020-03-30 22:56:33 -0400

    Express sessions 1.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -30,10 +32,70 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//When we use app.use we are basically defining our MIDDLEWARE.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// Adding cookie parser with secret.
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+//Setting up express session
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+/** 
+ * Before leting the client use the API or
+ *  Any of the public data we want to make sure
+ *  the user has access. We have to do this in our
+ *  MIDDLEWARE.
+ */
+
+function authenticateUser(request, response, next) {
+  console.log(request.session);
+
+  if (!request.session.user) {
+
+    var authHeader = request.headers.authorization;
+    if (!authHeader) {
+      var error = new Error('Unauthorized');
+      response.setHeader('WWW-Authenticate', 'Basic');
+      error.status = 401;
+      next(error);
+      return;
+    }
+
+    var authCredentials = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = authCredentials[0];
+    var password = authCredentials[1];
+
+    if (username == 'admin' && password == 'password') {
+      request.session.user = 'admin';
+      next();
+    } else {
+      var error = new Error('Unauthorized');
+      response.setHeader('WWWW-Authenticate', 'Basic');
+      error.status = 401;
+      next(error);
+    }
+  } else {
+    if (request.session.user == 'admin') {
+      next();
+    } else {
+      var error = new Error('Unauthorized');
+      error.status = 401;
+      next(error);
+    }
+  }
+}
+
+app.use(authenticateUser);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
